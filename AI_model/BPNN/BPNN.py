858518a4dfb97c1c4891f54ec9831f92038c2e8e
Mariiiiiio <godpig4019@gmail.com>
2023-07-06 16:22:56 +0800

    modify the BPNN file

--- AI_model/BPNN/BPNN.py
+++ AI_model/BPNN/BPNN.py
@@ -3,9 +3,10 @@ import numpy as np
 import time
 import sys  
 from sklearn.model_selection import train_test_split
-# sys.path.append('/Users/mariio/專題/論文專題/AI_model')  #for mac
+import pandas as pd
+sys.path.append('/Users/mariio/專題/論文專題/AI_model')  #for mac
 
-sys.path.append(r'C:\Users\USER\Desktop\University\Project\SmartComputing_Essay\AI_model') #for windows
+# sys.path.append(r'C:\Users\USER\Desktop\University\Project\SmartComputing_Essay\AI_model') #for windows
 
 from data_process import data_col
 
@@ -20,7 +21,24 @@ from data_process import data_col
     data6 : 年增率（不包含礦業與土石採取業) -目標
 '''
 
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
+def mean_squared_error(y_pred, y_true):
+    # One-hot encode y_true (i.e., convert [0, 1, 2] into [[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    y_true_one_hot = np.eye(output_size)[y_true]
+    
+    # Reshape y_true_one_hot to match y_pred shape
+    y_true_reshaped = y_true_one_hot.reshape(y_pred.shape)
+    
+    # Compute the mean squared error between y_pred and y_true_reshaped
+    error = ((y_pred - y_true_reshaped)**2).sum() / (2*y_pred.size)
+
+    return error
+
+def accuracy(y_pred, y_true):
+    acc = y_pred.argmax(axis=1) ==  y_true.argmax(axis=1)
+    return acc.mean()
 
 
 
@@ -45,4 +63,69 @@ if __name__ == '__main__':
     target_Year = np.array(data6)
     
     #Data prepare
-    x_train, x_test, y_train, y_test = train_test_split(data1_1ar, target_ori, test_size=0.2)
+    X_train, X_test, y_train, y_test = train_test_split(data1_1ar, target_ori, test_size=0.2)
+    print(y_train.size)
+    print(X_train.size)
+    print(X_train.shape[1])
+    
+    # Hyperparameters
+    learning_rate = 0.1
+    iterations = 5000
+    N = y_train.size
+    input_size = X_train.shape[1]
+    hidden_size = 2(input_size)
+    output_size = 1
+    
+    np.random.seed(10)
+    W1 = np.random.normal(scale=0.5, size=(input_size, hidden_size))
+    W2 = np.random.normal(scale=0.5, size=(hidden_size, output_size))
+    
+    # Helper functions
+    
+    
+    
+    results = pd.DataFrame(columns=["mse", "accuracy"])
+    
+    # Training loop
+    
+    for itr in range(iterations):
+        # Feedforward propagation
+        Z1 = np.dot(X_train, W1)
+        A1 = sigmoid(Z1)
+        Z2 = np.dot(A1, W2)
+        A2 = sigmoid(Z2)
+    
+        # Calculate error
+        mse = mean_squared_error(A2, y_train)
+        acc = accuracy(np.eye(output_size)[y_train], A2)
+        new_row = pd.DataFrame({"mse": [mse], "accuracy": [acc]})
+        results = pd.concat([results, new_row], ignore_index=True)
+    
+        # Backpropagation
+        E1 = A2 - np.eye(output_size)[y_train]
+        dW1 = E1 * A2 * (1 - A2)
+        E2 = np.dot(dW1, W2.T)
+        dW2 = E2 * A1 * (1 - A1)
+    
+        # Update weights
+        W2_update = np.dot(A1.T, dW1) / N
+        W1_update = np.dot(X_train.T, dW2) / N
+        W2 = W2 - learning_rate * W2_update
+        W1 = W1 - learning_rate * W1_update
+    
+    # Visualizing the results
+    
+    results.mse.plot(title="Mean Squared Error")
+    plt.show()
+    
+    results.accuracy.plot(title="Accuracy")
+    plt.show()
+    
+    # Test the model
+    
+    Z1 = np.dot(X_test, W1)
+    A1 = sigmoid(Z1)
+    Z2 = np.dot(A1, W2)
+    A2 = sigmoid(Z2)
+    test_acc = accuracy(np.eye(output_size)[y_test], A2)
+    print("Test accuracy: {}".format(test_acc))
